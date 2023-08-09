eed82dd06d80c0a565fd864ca189f5336ae3e8ca
Mort-Dono <mort@peak.watch>
2023-08-09 15:09:33 -0400

    adjusted content type fields and added conent type for home logo hdri

---
+++ src/api/logotexture/routes/logotexture.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * logotexture router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::logotexture.logotexture');
---
+++ src/api/logotexture/services/logotexture.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * logotexture service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::logotexture.logotexture');
